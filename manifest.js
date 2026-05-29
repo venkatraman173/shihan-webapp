20486ca4404aab687919f3989e08218df443ca65
venkatraman173 <162723109+venkatraman173@users.noreply.github.com>
2026-05-29 12:20:07 +0530

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,17 @@
+{
+  "short_name"; "DojoNetwork",
+  "name"; "Shihan Pradeep Babu Dojo Network",
+  "icons"; [
+    {
+      "src": "https://cdn-icons-png.flaticon.com/512/3003/3003202.png",
+      "type": "image/png",
+      "sizes": "512x512",
+      "purpose": "any maskable"
+    }
+  ],
+  "start_url"; "./index.html",
+  "background_color"; "#FAF6F0",
+  "theme_color"; "#D35400",
+  "display"; "standalone",
+  "orientation"; "portrait"
+}
